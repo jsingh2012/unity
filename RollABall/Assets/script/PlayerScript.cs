8759ea1bc760f251c1face31dc203a152a888a93
jasingh <jasingh@zynga.com>
2017-06-11 17:51:06 +0530

    RollTheBallCode

---
+++ RollABall/Assets/script/PlayerScript.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class PlayerScript : MonoBehaviour {
+
+	public float speed;
+	public Text countText;
+
+	private Rigidbody rb;
+
+	private int count;
+
+	void Start()
+	{
+		count = 0;
+		setCountText ();
+		rb = GetComponent<Rigidbody>();
+	}
+	void FixedUpdate()
+	{
+		float moveHorizontal  = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		Vector3 moveMovement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+
+		rb.AddForce(moveMovement * speed); 
+	}
+
+	void OnTriggerEnter(Collider other) {
+		if (other.gameObject.CompareTag ("PickUp"))
+		{
+			other.gameObject.SetActive (false);
+			count++;
+			setCountText ();
+		}
+	}
+
+	void setCountText()
+	{
+		countText.text = "Count: " + count.ToString();
+	}
+}
